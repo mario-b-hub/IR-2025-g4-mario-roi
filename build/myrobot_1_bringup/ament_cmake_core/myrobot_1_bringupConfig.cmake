51738639c4dc3cade8364945cb27d58f4bb4db6d
mario-b-hub <mariobadiaportillo@gmail.com>
2025-10-28 17:11:53 +0100

    Session3_hecha en sesion4

---
+++ build/myrobot_1_bringup/ament_cmake_core/myrobot_1_bringupConfig.cmake
@@ -0,0 +1,42 @@
+# generated from ament/cmake/core/templates/nameConfig.cmake.in
+
+# prevent multiple inclusion
+if(_myrobot_1_bringup_CONFIG_INCLUDED)
+  # ensure to keep the found flag the same
+  if(NOT DEFINED myrobot_1_bringup_FOUND)
+    # explicitly set it to FALSE, otherwise CMake will set it to TRUE
+    set(myrobot_1_bringup_FOUND FALSE)
+  elseif(NOT myrobot_1_bringup_FOUND)
+    # use separate condition to avoid uninitialized variable warning
+    set(myrobot_1_bringup_FOUND FALSE)
+  endif()
+  return()
+endif()
+set(_myrobot_1_bringup_CONFIG_INCLUDED TRUE)
+
+# output package information
+if(NOT myrobot_1_bringup_FIND_QUIETLY)
+  message(STATUS "Found myrobot_1_bringup: 0.0.0 (${myrobot_1_bringup_DIR})")
+endif()
+
+# warn when using a deprecated package
+if(NOT "" STREQUAL "")
+  set(_msg "Package 'myrobot_1_bringup' is deprecated")
+  # append custom deprecation text if available
+  if(NOT "" STREQUAL "TRUE")
+    set(_msg "${_msg} ()")
+  endif()
+  # optionally quiet the deprecation message
+  if(NOT ${myrobot_1_bringup_DEPRECATED_QUIET})
+    message(DEPRECATION "${_msg}")
+  endif()
+endif()
+
+# flag package as ament-based to distinguish it after being find_package()-ed
+set(myrobot_1_bringup_FOUND_AMENT_PACKAGE TRUE)
+
+# include all config extra files
+set(_extras "")
+foreach(_extra ${_extras})
+  include("${myrobot_1_bringup_DIR}/${_extra}")
+endforeach()
---
+++ build/myrobot_1_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ir-t12/ir_ws/src/session3/myrobot_1_description
+# Build directory: /home/ir-t12/ir_ws/src/build/myrobot_1_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
